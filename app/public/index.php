872e183580ed56ab5335e7c231969c159b82c45a
StJudeWasHere <707925+StJudeWasHere@users.noreply.github.com>
2024-11-17 11:55:05 +0100

    Style links

--- app/public/index.php
+++ app/public/index.php
@@ -52,20 +52,20 @@ if (!is_array($projects)) {
 
 <html>
 <meta charset="UTF-8">
-<title>WeDev Network Projects</title>
+<title>Projects Board - wwwdev.cc</title>
 <meta name="description" content="WebDev network community projects">
 <link rel="stylesheet" href="style.css?v=0.2">
 
 <body>
     <div class="content">
-        <h1>WeDev Network Projects</h1>
-
-        <p class="intro">
-            This is a showcase of projects that are seeking collaborators. If you're a developer looking to join forces, explore the projects listed here and reach out to the contact person to get started.
-        <br>
-            If you have your own project or idea that you'd like to share and find collaborators for, feel free to add it here. 
-            <a href="https://discord.gg/ypbQaP9e" target="_blank">Join us on Discord!</a>
-        </p>
+        <h1>Projects Board</h1>
+        <div class="intro">
+            <p>
+                This is a showcase of projects that are seeking collaborators. If you're a developer looking to join forces, explore the projects listed here and reach out to the contact person to get started.
+            </p><p>
+                If you have your own project or idea that you'd like to share and find collaborators for, feel free to add it here. 
+            </p>
+        </div>
 
         <section class="guide">
             <div class="guide-grid">
@@ -90,6 +90,7 @@ if (!is_array($projects)) {
                         <span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg></span>
                         <h3>Connect</h3>
                         <p>Use Discord usernames to connect with project owners and join their teams.</p>
+                        <a href="https://discord.gg/KJFGcyYVwN" target="_blank">Join us on Discord!</a>
                     </div>
                 </div>
             </div>
@@ -135,7 +136,12 @@ if (!is_array($projects)) {
         <?php } ?>
     </div>
     <footer>
-        <small>WebDev Network 2024</small>
+        <p>
+            <a href="https://github.com/webdev-network" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+            </a>
+        </p>
+        <small>wwwdev.cc 2024</small>
     </footer>
 </body>
 
