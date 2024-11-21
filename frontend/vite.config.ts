9b6686a427dc83d716738526ce0cae2d62090484
Frederick <125679913+FredMT@users.noreply.github.com>
2024-11-21 18:41:31 +0100

    Serve frontend on port 3000

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,24 @@
 /// <reference types="vitest/config" />
-import path from "path";
-import { defineConfig } from "vite";
-import viteReact from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import path from 'path'
+import { defineConfig } from 'vite'
+import viteReact from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [TanStackRouterVite(), viteReact()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./vitest.setup.mjs",
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.mjs',
     include: ['**/*.{test,spec}.{ts,tsx}'],
     exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
   },
-});
+})
