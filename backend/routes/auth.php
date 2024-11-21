616b50b4cb482472d39d8fbc2ec78dd00e60417e
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2024-11-21 02:48:54 -0500

    chore: modify route files to handle auth endpoints

--- backend/routes/auth.php
+++ backend/routes/auth.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::post('/register', [RegisteredUserController::class, 'store'])
                 ->middleware('guest')
                 ->name('register');
@@ -25,13 +26,13 @@ Route::post('/reset-password', [NewPasswordController::class, 'store'])
                 ->name('password.store');
 
 Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
-                ->middleware(['auth', 'signed', 'throttle:6,1'])
+                ->middleware(['auth:sanctum', 'signed', 'throttle:6,1'])
                 ->name('verification.verify');
 
 Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-                ->middleware(['auth', 'throttle:6,1'])
+                ->middleware(['auth:sanctum', 'throttle:6,1'])
                 ->name('verification.send');
 
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->middleware('auth')
+                ->middleware('auth:sanctum')
                 ->name('logout');
